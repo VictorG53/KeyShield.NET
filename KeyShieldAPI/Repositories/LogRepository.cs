375d28f0c17c0822d190de312731d3c0c3d27b7a
Julien <aski@localhost>
2025-05-23 09:58:22 +0200

    feat(Log): ajout des logs fonctionnel

--- KeyShieldAPI/Repositories/LogRepository.cs
+++ KeyShieldAPI/Repositories/LogRepository.cs
@@ -22,14 +22,44 @@ public class LogRepository(KeyShieldDbContext dbContext, UtilisateurService util
     
     public async Task<Log> CreateLogAsync(LogDTORequest logDto)
     {
-        Console.WriteLine("Log enregistré");
+        // Récupération du type d'action
+        var actionType = await dbContext.ActionTypes
+            .FirstOrDefaultAsync(a => a.Identifiant == logDto.ActionTypeIdentifiant);
+
+        if (actionType == null)
+            throw new Exception("Type d'action non trouvée");
+
+        // Récupération de l'utilisateur
+        var user = await dbContext.Utilisateurs
+            .FirstOrDefaultAsync(u => u.Identifiant == logDto.UtilisateurCreateurIdentifiant);
+
+        if (user == null)
+            throw new Exception("Utilisateur introuvable");
+        
+        // Console.WriteLine(user.EntraId);
+        
 
         var log = new Log
         {
             Identifiant = Guid.NewGuid(),
-            Horodatage = DateTime.UtcNow,
+            // Horodatage = new DateTime(),
+            Horodatage = DateTime.Now,
             Message = logDto.Message,
+            UtilisateurCreateurIdentifiant = logDto.UtilisateurCreateurIdentifiant,
+            UtilisateurPartageIdentifiant = null,
+            ActionTypeIdentifiant = logDto.ActionTypeIdentifiant,
+            CoffreIdentifiant = null,
+            EntreeIdentifiant = null,
+            DonneeIdentifiant = null,
+            Utilisateur = user,
+            UtilisateurPartage = null,
+            Coffre = null,
+            Entree = null,
+            Donnee = null
         };
+        
+        await dbContext.Logs.AddAsync(log);
+        await dbContext.SaveChangesAsync();
         
         return log;
     }
